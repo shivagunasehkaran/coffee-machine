4f7e4a6adafd23359780b864c5c0364b49d56089
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-27 02:01:23 +0530

    Create routes file

---
+++ src/routes/Routes.js
@@ -0,0 +1,6 @@
+// page names - public
+export const pageNamePublicStack = 'PublicStack';
+export const pageNameCoffeeMachine = 'CoffeeMachine';
+export const pageNameCoffeeTypes = 'CoffeeTypes';
+export const pageNameCoffeeSize = 'CoffeeSize';
+export const pageNameCoffeeExtras = 'CoffeeExtras';
