9fbde06f0f072dffaea9c3a3d6295fa906379eb4
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-28 00:01:53 +0530

    Write unit test cases for coffee details component

---
+++ __tests__/components/CoffeeDetails.test.js
@@ -0,0 +1,24 @@
+import enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+
+import CoffeeDetails from '../../src/components/CoffeeDetails';
+
+enzyme.configure({adapter: new Adapter()});
+
+describe('coffee details component validation', () => {
+  it('renders correctly with coffee detail elments', async () => {
+    const wrapper = shallow(<CoffeeDetails />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render with image elements', async () => {
+    const wrapper = shallow(<CoffeeDetails />);
+    expect(wrapper.find('Image')).toHaveLength(1);
+  });
+
+  it('should render with text elements', async () => {
+    const wrapper = shallow(<CoffeeDetails />);
+    expect(wrapper.find('Text')).toHaveLength(1);
+  });
+});
