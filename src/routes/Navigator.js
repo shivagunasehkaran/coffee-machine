b980c679cd465ffde1bd9c4e8e4fd87ca1f1da7e
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-28 00:46:26 +0530

    Add stack and style for back button

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -2,15 +2,29 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 // presentational component imports
-import * as ROUTES from './Routes';
 import CoffeeMachine from '../views/CoffeeMachine';
 import CoffeeTypes from '../views/CoffeeTypes';
+import CoffeeSizes from '../views/CoffeeSizes';
+// style imports
+import {ColourPalette} from '../assets/styles/ColourPalette';
+// route imports
+import * as ROUTES from './Routes';
+// util  imports
+import {ConstantText} from '../utills/ConstantText';
 
 const PublicStack = createStackNavigator();
 
 export const PublicRoutes = () => {
   return (
-    <PublicStack.Navigator>
+    <PublicStack.Navigator
+      screenOptions={{
+        headerTintColor: ColourPalette.black,
+        headerBackTitleStyle: {
+          fontSize: 16,
+          fontFamily: ConstantText.font_family,
+          fontWeight: ConstantText.font_bold,
+        },
+      }}>
       <PublicStack.Screen
         key={ROUTES.pageNameCoffeeMachine}
         name={ROUTES.pageNameCoffeeMachine}
@@ -25,6 +39,16 @@ export const PublicRoutes = () => {
         component={CoffeeTypes}
         options={{
           headerShown: false,
+        }}
+      />
+      <PublicStack.Screen
+        key={ROUTES.pageNameCoffeeSizes}
+        name={ROUTES.pageNameCoffeeSizes}
+        component={CoffeeSizes}
+        options={{
+          title: '',
+          headerShown: true,
+          headerBackTitle: ConstantText.coffee_device_id_title,
         }}
       />
     </PublicStack.Navigator>
