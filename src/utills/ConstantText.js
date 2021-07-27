93cf69ba6060214dd29c75fd9808bb94b1ec94a5
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-27 22:38:30 +0530

    Update static text

--- src/utills/ConstantText.js
+++ src/utills/ConstantText.js
@@ -1,8 +1,10 @@
 // applicaion text constants
 export const ConstantText = {
-  nfc_title: 'Dark Roasted Beans',
-  nfc_subTitle: 'Tap the machine to start',
   toast_success: 'success',
   toast_connected: 'Device Connected.',
   toast_bottom: 'bottom',
+  coffee_machine_title: 'Dark Roasted Beans',
+  coffee_machine_subTitle: 'Tap the machine to start',
+  coffee_device_id_title: 'Brew with Lex',
+  coffee_types_subTitle: 'Select your style',
 };
