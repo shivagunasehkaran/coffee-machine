534945c538b56cb4149f757f1ce2fc4b7bfa1d98
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-27 18:11:42 +0530

    Create constants for API url

---
+++ src/services/Constants.js
@@ -0,0 +1,8 @@
+// Base URL & API key for further API calls
+const BASE_URL = 'https://darkroastedbeans.coffeeit.nl/';
+const API_KEY = '60ba1ab72e35f2d9c786c610';
+
+// API URL
+export const GET_URL = {
+  COFFEE_DETAILS_URL: `${BASE_URL}coffee-machine/${API_KEY}`,
+};
