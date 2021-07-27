b6f0fb226e7beb739bcfeec4b90268979dc31b42
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-27 22:44:38 +0530

    Make API call and get coffee details

--- src/views/CoffeeTypes/CoffeeTypes.js
+++ src/views/CoffeeTypes/CoffeeTypes.js
@@ -1,12 +1,25 @@
 // library imports
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+// API imports
+import {useFetchCoffeeAPI} from '../../services/Services';
 // util imports
 import {ConstantText} from '../../utills/ConstantText';
 // style imports
 import {styles} from './CoffeeTypes.style';
 
 const CoffeeTypes = props => {
+  // call API one time
+  useEffect(() => {
+    getCoffeeDetails();
+  }, []);
+
+  // get coffee details using API
+  async function getCoffeeDetails() {
+    let data = await useFetchCoffeeAPI();
+    console.log('data -- >', data);
+  }
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
