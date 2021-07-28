1c9a982d07d9542c75275b529c8e204db2bf06cd
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-28 22:57:21 +0530

    Add coverage for flat list in coffee extras

---
+++ __tests__/views/CoffeeExtras.test.js
@@ -0,0 +1,32 @@
+// library imports
+import React from 'react';
+import enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// page imports
+import CoffeeExtras from '../../src/views/CoffeeExtras';
+
+enzyme.configure({adapter: new Adapter()});
+
+describe('Coffee types screen', () => {
+  it('renders correctly with coffee types', async () => {
+    const wrapper = shallow(<CoffeeExtras />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('coffee extras screen : flatlist', () => {
+  it('should flatlist return keyExtractor correctly', () => {
+    const item = [
+      {
+        id: '1',
+        name: 'sugar',
+      },
+      {
+        id: '2',
+        name: 'milk',
+      },
+    ];
+    const wrapper = shallow(<Home item={item} />);
+    expect(wrapper.find('FlatList').length).toEqual(1);
+  });
+});
