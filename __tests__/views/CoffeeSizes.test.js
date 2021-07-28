31df8ae1e72fdf9998381aa9ff8fa0db82311350
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-28 23:00:58 +0530

    Do unit test for coffee sizes

---
+++ __tests__/views/CoffeeSizes.test.js
@@ -0,0 +1,36 @@
+// library imports
+import enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+// page imports
+import CoffeeSizes from '../../src/views/CoffeeSizes';
+
+enzyme.configure({adapter: new Adapter()});
+
+describe('Coffee sizes screen', () => {
+  it('renders correctly with coffee sizes', async () => {
+    const wrapper = shallow(<CoffeeSizes />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('coffee sizes screen : flatlist', () => {
+  it('should flatlist return keyExtractor correctly', () => {
+    const item = [
+      {
+        id: '1',
+        name: 'large',
+      },
+      {
+        id: '2',
+        name: 'small',
+      },
+      {
+        id: '2',
+        name: 'medium',
+      },
+    ];
+    const wrapper = shallow(<Home item={item} />);
+    expect(wrapper.find('FlatList').length).toEqual(1);
+  });
+});
