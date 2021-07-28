430097b9caf2a5c8f589d4728f2da42e516a82ed
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-28 14:57:18 +0530

    Wrap context provider for whole app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import React from 'react';
 import type {Node} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
-// container component
+// container imports
 import PrimaryContainer from './containers';
+// provider imports
+import CoffeeProvider from './provider/CoffeeProvider';
 
 const App = (): Node => {
   return (
     <NavigationContainer>
-      <PrimaryContainer />
+      <CoffeeProvider>
+        <PrimaryContainer />
+      </CoffeeProvider>
       <Toast ref={ref => Toast.setRef(ref)} />
     </NavigationContainer>
   );
